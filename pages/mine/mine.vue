e355382539189510d481d75122ce2dfec5366c87
Argrule <99395773+Argrule@users.noreply.github.com>
2023-08-07 11:25:42 +0800

    :sparkles: feat: integrate profile page with api

--- pages/mine/mine.vue
+++ pages/mine/mine.vue
@@ -2,17 +2,19 @@
 import { ref, reactive,onMounted,onBeforeMount } from 'vue'
 import request from '../../request/request'
 onMounted(async ()=>{
-	console.log('onMounted!!!!!!!!!')
-	// request('/api/personal-center/detail','get',)
-	const res = await request('https://api.apiopen.top/api/sentences','get',undefined,undefined,true)
-	console.log("!!!  ",res)
+	// console.log('onMounted!!!!!!!!!')	
+	// const res = await request('https://api.apiopen.top/api/sentences','get',undefined,undefined,true)
+	// console.log("test request!!!  ",res)
 })
-onBeforeMount(()=>{
-	console.log('onBeforeMount!!!!!!!!!')
+onBeforeMount(async ()=>{	
+	const {data:res_data} =await request('/api/personal-center/detail','get')	
+	console.log('onBeforeMount!!!!!!!!!',res_data)
+	state.data = res_data
 })
 
 let isLogin = ref(true)
-const profileData = reactive({
+// mock data
+const profileData = {
 	address: "广东省广州市",
 	age: 38,
 	avatarUrl: "http://dummyimage.com/100x100",
@@ -26,11 +28,9 @@ const profileData = reactive({
 	phone: "13302238687",
 	position: "运营部成员",
 	userId: 5,
-})
-const requestRES = reactive({
-	"code": "00000",
-	"data": profileData,
-	"message": "请求正常",
+}
+const state = reactive({	
+	data: profileData
 })
 function jumpPage() {
 	console.log('jumpPage function')
@@ -42,11 +42,11 @@ function jumpPage() {
 		<!-- 个人信息 -->
 		<view class="profile-info">
 			<view class="image-wrap">
-				<u-avatar :text="profileData.name[0]" fontSize="25" randomBgColor size="50"></u-avatar>
-				<!-- <u-avatar :src="profileData.avatarUrl" fontSize="28" size="50"></u-avatar> -->
+				<!-- <u-avatar :text="state.data.name[0]" fontSize="25" randomBgColor size="50"></u-avatar> -->
+				<u-avatar :src="state.data.avatarUrl" fontSize="28" size="50"></u-avatar>
 			</view>
 			<view class="info-wrap">
-				<view class="profile-name">{{ profileData.name }}</view>
+				<view class="profile-name">{{ state.data.name }}</view>
 				<view class="iconfont icon-xiugaixinxi" @click="jumpPage">修改个人信息</view>
 			</view>			
 		</view>
@@ -54,11 +54,11 @@ function jumpPage() {
 		<!-- 其他 -->
 		<view class="profile-other">
 			<view class="profile-other">
-				<view class="profile-item wrap"><text>职位</text><text>{{ profileData.position }}</text></view>
-				<view class="profile-item wrap"><text>电话</text><text>{{ profileData.phone }}</text></view>
-				<view class="profile-item wrap"><text>教育经历</text><text>{{ profileData.education }}</text></view>
-				<view class="profile-item wrap"><text>年龄</text><text>{{ profileData.age }}</text></view>
-				<view class="profile-item wrap"><text>地址</text><text>{{ profileData.address }}</text></view>
+				<view class="profile-item wrap"><text>职位</text><text>{{ state.data.position }}</text></view>
+				<view class="profile-item wrap"><text>电话</text><text>{{ state.data.phone }}</text></view>
+				<view class="profile-item wrap"><text>教育经历</text><text>{{ state.data.education }}</text></view>
+				<view class="profile-item wrap"><text>年龄</text><text>{{ state.data.age }}</text></view>
+				<view class="profile-item wrap"><text>地址</text><text>{{ state.data.address }}</text></view>
 			</view>
 			<navigator class="wrap wrap1" url="#" open-type="navigate">
 				<text>意见反馈</text>
