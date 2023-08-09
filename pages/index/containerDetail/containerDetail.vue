d3a0297883548447169c2e1e723f62303fe437d1
lizhengdong <3209562144@qq.com>
2023-08-09 14:42:04 +0800

    :zap: 优化代码结构

--- pages/index/containerDetail/containerDetail.vue
+++ pages/index/containerDetail/containerDetail.vue
@@ -1,77 +1,43 @@
 <template>
-  <view class="container">
-    <view class="container-info">
-      <view class="info-head">集装箱信息</view>
-      <view class="info-items">
-        <view class="info-item">
-          <view class="item-title">集装箱号：</view>
-          <view class="item-content">{{ containerNumber }}</view>
-        </view>
-        <view class="info-item">
-          <view class="item-title">箱主：</view>
-          <view class="item-content">{{ containerOwner }}</view>
-        </view>
-        <view class="info-item">
-          <view class="item-title">箱型：</view>
-          <view class="item-content">{{ containerType }}</view>
-        </view>
-        <view class="info-item">
-          <view class="item-title">状态：</view>
-          <view class="item-content">{{ containerState }}</view>
-        </view>
-        <view class="info-item">
-          <view class="item-title">Tare(kg)：</view>
-          <view class="item-content">{{ containerTare }}</view>
-        </view>
-        <view class="info-item">
-          <view class="item-title">Max.Payload(kg)：</view>
-          <view class="item-content">{{ containerMaximumLoad }}</view>
-        </view>
-        <view class="info-item">
-          <view class="item-title">破损情况：</view>
-          <view class="item-content">{{ containerBreakage }}</view>
-        </view>
-        <view class="info-item">
-          <view class="item-title">其他：</view>
-          <view class="item-content">{{ containerOther }}</view>
-        </view>
-      </view>
-    </view>
-    <view class="operation-form">
+  <div class="container">
+    <div class="container-info">
+      <div class="info-head">集装箱信息</div>
+      <div class="info-items">
+        <div class="info-item" v-for="item in infoItems" :key="item.title">
+          <div class="item-title">{{ item.title }}</div>
+          <div class="item-content">{{ item.content != null ? item.content.value : '暂无数据' }}</div>
+        </div>
+      </div>
+    </div>
+    <div class="operation-form">
       <uni-table ref="table" :loading="loading" border stripe emptyText="暂无更多数据" @selection-change="selectionChange">
         <uni-tr>
           <uni-th width="40" align="center">操作</uni-th>
           <uni-th width="80" align="center">操作地点</uni-th>
           <uni-th width="100" align="center">时间</uni-th>
-          <!-- <uni-th width="40" align="center">时间</uni-th> -->
         </uni-tr>
         <uni-tr v-for="(item, index) in tableData" :key="index">
           <uni-td align="center">
-            <view class="td-item">{{ item.operation }}</view>
+            <div class="td-item">{{ item.operation }}</div>
           </uni-td>
           <uni-td align="center">
-            <view class="td-item">{{ item.location }}</view>
+            <div class="td-item">{{ item.location }}</div>
           </uni-td>
           <uni-td align="center">
-            <view class="td-item">{{ item.operationTime }}</view>
+            <div class="td-item">{{ item.operationTime }}</div>
           </uni-td>
-          <!-- <uni-td align="center">
-            <view class="td-item">{{ item.operateTime }}</view>
-          </uni-td> -->
         </uni-tr>
       </uni-table>
-      <view class="uni-pagination-box">
+      <div class="uni-pagination-box">
         <uni-pagination show-icon :page-size="pageSize" :current="pageCurrent" :total="total" @change="change" />
-      </view>
-    </view>
-  </view>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { ref, onMounted  } from 'vue';
-// import importedTableData from './tableData.js';
-import {tsFormat} from '../../../utils/time.js';
-
+import { ref, onMounted } from 'vue';
+import { tsFormat } from '../../../utils/time.js';
 
 const containerMaximumLoad = ref('27280');
 const containerTare = ref('3200');
@@ -83,99 +49,82 @@ const containerOwner = ref('MAERSK 马士基');
 const containerNumber = ref('PONU 2932473');
 const searchVal = ref('');
 const tableData = ref([]);
-const pageSize = 3;
+const pageSize = 2;
 const pageCurrent = ref(1);
 const total = ref(0);
 const loading = ref(false);
 const selectedIndexs = ref([]);
-
 const dataParam = ref('');
 
+const infoItems = [
+  { title: '集装箱号：', content: containerNumber },
+  { title: '箱主：', content: containerOwner },
+  { title: '箱型：', content: containerType },
+  { title: '状态：', content: containerState },
+  { title: 'Tare(kg)：', content: containerTare },
+  { title: 'Max.Payload(kg)：', content: containerMaximumLoad },
+  { title: '破损情况：', content: containerBreakage },
+  { title: '其他：', content: containerOther },
+];
+
 onMounted(() => {
-  // const pages = getCurrentPages();
-  // const currentPage = pages[pages.length - 1];
-  // const query = currentPage.options;
-  // console.log('currentPage.options',currentPage.options);
-  // if (query.data) {
-    // 解码并解析传递的数据
-    dataParam.value = uni.getStorageSync("containerData");
-    //清空containerData
-    uni.setStorageSync('containerData', '');
-    const containerInfo=dataParam.value.containerInfo;
-    // console.log('dataParam.value.containerOperations',dataParam.value.containerOperations);
-    // console.log('containerInfo',containerInfo);
-    if(containerInfo.damage){
-      containerBreakage.value=containerInfo.damage;
-    }
-    
-    containerNumber.value=containerInfo.number;
-    containerOwner.value=containerInfo.owner;
-    containerState.value=containerInfo.status;
-    containerType.value=containerInfo.type;
-    containerMaximumLoad.value=containerInfo.bearing;
-    containerOther.value=containerInfo.other;
-    containerTare.value=containerInfo.weight;//Todo:确认是否为weight字段
-  // }
+  dataParam.value = uni.getStorageSync('containerData');
+  uni.setStorageSync('containerData', '');
+  const containerInfo = dataParam.value.containerInfo;
+  if (containerInfo.damage) {
+    containerBreakage.value = containerInfo.damage;
+  }
+  console.log('containerInfo.number',containerInfo.number);
+  containerNumber.value = containerInfo.number;
+  containerOwner.value = containerInfo.owner;
+  containerState.value = containerInfo.status;
+  containerType.value = containerInfo.type;
+  containerMaximumLoad.value = containerInfo.bearing;
+  if(containerInfo.other==null){
+    containerInfo.other='暂无数据'
+  }
+  containerOther.value = containerInfo.other;
+  containerTare.value = containerInfo.weight;
   getData(1);
+  console.log('containerNumber',containerNumber.value);
 });
 
-
-// 分页触发
 function change(e) {
   getData(e.current);
 }
 
-// 搜索
-function search() {
-  getData(1, searchVal.value);
-}
-
-// 获取数据
 function getData(pageNum, value = '') {
   loading.value = true;
   pageCurrent.value = pageNum;
-  request({
-    pageSize: pageSize,
-    pageCurrent:pageNum,
-    value: value,
-    success: (res) => {
-      tableData.value = res.data;
-      total.value = res.total;
-      loading.value = false;
-    },
-  });
+  // Simulating request using setTimeout
+  setTimeout(() => {
+    const simulatedData = getSimulatedData(dataParam.value.containerOperations, pageSize, pageNum, value);
+    tableData.value = simulatedData.data;
+    total.value = simulatedData.total;
+    loading.value = false;
+  }, 500);
 }
 
-// 伪request请求
-function request(options) {
-  const { pageSize, pageCurrent, success, value } = options;
-  let tableDataLength = dataParam.value.containerOperations.length;
-  let data = dataParam.value.containerOperations.filter((item, index) => {
+function getSimulatedData(data, pageSize, pageCurrent, value) {
+  let tableDataLength = data.length;
+  let filteredData = data.filter((item, index) => {
     const idx = index - (pageCurrent - 1) * pageSize;
     return idx < pageSize && idx >= 0;
   });
+
   if (value) {
-    data = [];
-    dataParam.value.containerOperations.forEach((item) => {
-      if (item.name.indexOf(value) !== -1) {
-        data.push(item);
-        // console.log('item',item);
-      }
-    });
-    tableDataLength = data.length;
+    filteredData = data.filter(item => item.name.indexOf(value) !== -1);
+    tableDataLength = filteredData.length;
   }
-  // console.log('requestdata',data);
-  const transformedArray = data.map(({ operationTime, ...rest }) => {
+
+  const transformedData = filteredData.map(({ operationTime, ...rest }) => {
     return { operationTime: tsFormat(operationTime), ...rest };
   });
-  // console.log('transformedArray',transformedArray);
-  setTimeout(() => {
-    typeof success === 'function' &&
-      success({
-        data: transformedArray,
-        total: tableDataLength,
-      });
-  }, 500);
+
+  return {
+    data: transformedData,
+    total: tableDataLength,
+  };
 }
 </script>
 
